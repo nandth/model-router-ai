3438e29644d104670af2f5ad860ba515df89c753
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2026-02-07 12:51:51 -0500

    fix: fixed errors with electron build

--- client/electron/main.cjs
+++ client/electron/main.cjs
@@ -1,10 +1,17 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const { Menu } = require("electron");
+const { globalShortcut } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: false,
+    },
   });
 
   if (!app.isPackaged) {
@@ -14,8 +21,24 @@ function createWindow() {
   }
 }
 
+const template = [
+  {
+    label: "File",
+    submenu: [{ role: "quit" }],
+  },
+];
+
+const menu = Menu.buildFromTemplate(template);
+
+Menu.setApplicationMenu(null);
+
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  globalShortcut.register("CommandOrControl+Shift+I", () => {});
+  globalShortcut.register("F12", () => {});
 });
